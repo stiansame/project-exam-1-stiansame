f25b7c27b40b7ff8be025f4d48971a5ed435ef7c
stiansame <stianrostad@gmail.com>
2024-11-04 21:32:28 +0100

    blog page
    html, css Js and api call

--- js/getBlogs.js
+++ js/getBlogs.js
@@ -1,24 +1,35 @@
 //imports
-import { blogListcontainer } from "./script";
-import { blogListUrl } from "./script";
-
+import { blogListcontainer } from "../js/script.js";
+import { blogListUrl } from "../js/script.js";
+import { message } from "../js/script.js";
 
 //call API to get blogs
 async function getBloglist() {
-    try {
-        const response = await fetch(blogListUrl);
-        const json = await response.json();
+  try {
+    const response = await fetch(blogListUrl);
+    const json = await response.json();
 
-        blogListcontainer.innerHTML = "";
+    blogListcontainer.innerHTML = "";
 
-        const blogList = json;
-        console.log({ blogList })
-        
-        blogList.forEach(function (blog) {
-        blogListcontainer.innerHTML += `<div class="blogExcerpt">
-                                            <p>HEADER: ${title.rendered}                                
-            </div > `
-        })
+    const blogList = json;
+    console.log({ blogList });
 
-    }
+    blogList.forEach((blog) => {
+      const categories = blog._embedded["wp:term"][0];
+      const catString = categories.map((cat) => cat.name).join(", ");
+      console.log(catString);
+
+      blogListcontainer.innerHTML += `<div class="shortBlog">
+                <img class="blogList_img" src="${blog._embedded["wp:featuredmedia"][0].source_url}">
+                <div class="blogCat">${catString}</div>
+                <h2>${blog.title.rendered}</h2>
+                <p>${blog.excerpt.rendered}</p>
+                <div class="blogAuthor">${blog._embedded.author[0].name}</div>
+                </div>`;
+    });
+  } catch (error) {
+    blogListcontainer.innerHTML = message;
+  }
 }
+
+await getBloglist();
--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 //imports here
 
+import { createMessage } from "../js/message.js";
+
 //URL for api call
 export const blogListUrl =
-  "https://stianrostad.no/wordpress/wp-json/wp/v2/posts?per_page=10";
+  "https://stianrostad.no/wordpress/wp-json/wp/v2/posts?_embed&per_page=10";
 
 //Define DIVS to display API-call results
+export const blogListcontainer = document.querySelector(".bloglist");
+
+//Display message
+export const message = createMessage();
