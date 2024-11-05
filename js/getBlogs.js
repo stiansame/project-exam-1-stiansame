0748104c9a679ac7ba2fb58d09fa7803afe630fb
stiansame <stianrostad@gmail.com>
2024-11-05 15:29:28 +0100

    css

--- js/getBlogs.js
+++ js/getBlogs.js
@@ -19,14 +19,20 @@ async function getBloglist() {
       const catString = categories.map((cat) => cat.name).join(", ");
       console.log(catString);
 
+      const date = new Date(blog.date);
+      const dateTimeFormatter = new Intl.DateTimeFormat("NO", {
+        dateStyle: "long",
+      });
+      const formatDate = dateTimeFormatter.format(date);
+
       blogListcontainer.innerHTML += `<div class="shortBlog">
                 <img class="blogList_img" src="${blog._embedded["wp:featuredmedia"][0].source_url}">
                 <div class="blogCat">${catString}</div>
                 <div class="blogListHeader"><h2>${blog.title.rendered}</h2></div>
-                <Div class="blogListExcerpt"><p>${blog.excerpt.rendered}</p></div>
-                <div class="blogAuthor"><img class="bloglistAvatar" src="${blog._embedded.author[0].avatar_urls[24]}"> ${blog._embedded.author[0].name}</div>
-                <div> ${blog.date}</div>
-                <div>
+                <div class="blogListExcerpt"><p>${blog.excerpt.rendered}</p></div>
+                <div class="blogAuthor"><img class="bloglistAvatar" src="${blog._embedded.author[0].avatar_urls[24]}"> ${blog._embedded.author[0].name}
+                <div> ${formatDate}</div>
+                </div>
                 </div>`;
     });
   } catch (error) {
